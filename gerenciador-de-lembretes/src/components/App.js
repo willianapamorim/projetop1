d9e8a97c599854f37e38ec334ed9fd1faa0a0863
Willian Amorim <110067851+WillianApAmorim@users.noreply.github.com>
2024-04-14 13:09:56 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ gerenciador-de-lembretes/src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+
+  return (
+    <div className="container">
+      <div className="border mt-5 px-3 py-4" style={{ minHeight: '200px' }}>
+        <h1>Hello, lembretes</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
